758eb744cbd217cb857b8ad40bbb5b44133ff452
Red (RedFox0x20) <redfox0x20@gmail.com>
2022-02-22 20:43:41 +0000

    Modals
    Created a demo page for displaying Modals to the user.

---
+++ pages/Modals/Modals.js
@@ -0,0 +1,9 @@
+const myModal = document.getElementById("myModal");
+
+function modal_show() {
+    myModal.classList.remove("hidden");
+}
+
+function modal_hide() {
+    myModal.classList.add("hidden");
+}
